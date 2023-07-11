02e3b1940469e283935f6de8ec44fe802b7338c2
Xavi661 <javierpadin661@gmail.com>
2023-07-11 01:12:53 -0500

    eliminacion de la base de datos y peticon de patron

--- app/src/main/java/com/example/minimarket/DB/DBHelper.java
+++ app/src/main/java/com/example/minimarket/DB/DBHelper.java
@@ -10,7 +10,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
     private static final int DATABASE_VERSION = 4;
-    private static final String DATABASE_NOMBRE = "tienda.db";
+    public static final String DATABASE_NOMBRE = "tienda.db";
     public static final String TABLA_PRODUCTOS = "t_productos";
 
 
--- app/src/main/java/com/example/minimarket/MainActivity.java
+++ app/src/main/java/com/example/minimarket/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.minimarket;
 
 
 import android.annotation.SuppressLint;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -13,8 +17,10 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.minimarket.DB.DBHelper;
+import com.example.minimarket.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -23,7 +29,8 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.minimarket.databinding.ActivityMainBinding;
+
+import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     ActivityMainBinding binding;
 
-    Button crear_db;
+    Button crear_db, eliminar_bd;
+
+    private static final int REQUEST_CODE_CONFIRM_PATTERN = 1;
 
 
     @Override
@@ -83,27 +92,108 @@ public class MainActivity extends AppCompatActivity {
 
         // Establecer el tema según el estado almacenado en las preferencias compartidas
         setDayNight(isDarkMode);
-
-        crear_db=findViewById(R.id.CREARDB);
-
+        eliminar_bd = findViewById(R.id.ELIMINARDB);
+        crear_db = findViewById(R.id.CREARDB);
         crear_db.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DBHelper dbHelper = new DBHelper(MainActivity.this);
-                SQLiteDatabase db=dbHelper.getWritableDatabase();
-                if(db!=null){
-                    Toast.makeText(MainActivity.this,"BASE DE DATOS CREADA",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(MainActivity.this,"ERROR AL CREAR BASE DE DATOS",Toast.LENGTH_SHORT).show();
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                if (db != null) {
+                    Toast.makeText(MainActivity.this, "BASE DE DATOS CREADA", Toast.LENGTH_SHORT).show();
+                    validarDB();
+                } else {
+                    Toast.makeText(MainActivity.this, "ERROR AL CREAR BASE DE DATOS", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
 
-
-
+        //pedir patron para eliminar
+        eliminar_bd = findViewById(R.id.ELIMINARDB);
+        eliminar_bd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authenticatePattern();
+            }
+        });
 
     }
+
+    private void authenticatePattern() {
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        if (keyguardManager.isKeyguardSecure()) {
+            Intent intent = keyguardManager.createConfirmDeviceCredentialIntent("Desbloquea para continuar", "Ingresa tu patrón");
+            if (intent != null) {
+                startActivityForResult(intent, REQUEST_CODE_CONFIRM_PATTERN);
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CONFIRM_PATTERN) {
+            if (resultCode == RESULT_OK) {
+                // El patrón de desbloqueo fue ingresado correctamente
+                // Aquí puedes realizar las acciones que desees después de la autenticación exitosa
+                // Por ejemplo, puedes habilitar el botón que deseas
+                eliminar_bd.setEnabled(true);
+                eliminarBaseDeDatos();
+
+            } else {
+                // El patrón de desbloqueo no fue ingresado correctamente
+                // Aquí puedes manejar el caso cuando la autenticación falla
+            }
+        }
+
+    }
+
+    public void eliminarBaseDeDatos() {
+        boolean eliminadaa = deleteDatabase(DBHelper.DATABASE_NOMBRE);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("¿DESEA ELIMINAR LA BASE DE DATOS?")
+                .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //accion si
+                        if (eliminadaa) {
+                            Toast.makeText(MainActivity.this, "Base de datos eliminada", Toast.LENGTH_SHORT).show();
+                            validarDB();
+//                            vistavista();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error al eliminar la base de datos", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //accion no
+                    }
+                }).show();
+    }
+
+
+    public void validarDB() {
+        boolean databaseExists = checkDatabaseExists();
+        if (databaseExists) {
+            crear_db.setVisibility(View.GONE);
+            eliminar_bd.setVisibility(View.VISIBLE);
+        } else {
+            Toast.makeText(MainActivity.this, "Cree la base de datos", Toast.LENGTH_SHORT).show();
+            crear_db.setVisibility(View.VISIBLE);
+            eliminar_bd.setVisibility(View.GONE);
+
+
+        }
+    }
+
+    private boolean checkDatabaseExists() {
+        File databaseFile = getApplicationContext().getDatabasePath(DBHelper.DATABASE_NOMBRE);
+        return databaseFile.exists();
+    }
+
     @SuppressLint("UseCompatLoadingForDrawables")
     private void setDayNight(boolean isDarkMode) {
         if (isDarkMode) {
@@ -125,15 +215,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
